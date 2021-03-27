244e1bfdb9a0884da72823f4f99c6587e45baeb6
Adv35 <78689485+Adv35@users.noreply.github.com>
2021-03-27 14:43:25 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+let counter = 0
+let trips = 0
+for (let index = 0; index < 10; index++) {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
+    basic.pause(2000)
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(1000)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 40)
+    basic.pause(2000)
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(1000)
+    trips += 1
+    serial.writeValue("trips", trips)
+    if (trips == 0 || (trips == 3 || (trips == 6 || trips == 9))) {
+        counter = 45
+        for (let index = 0; index < 17; index++) {
+            counter += -2
+            serial.writeValue("count", counter)
+        }
+    }
+}
 basic.forever(function () {
 	
 })
